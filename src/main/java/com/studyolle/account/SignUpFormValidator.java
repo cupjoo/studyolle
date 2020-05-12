e8c861c0a23e7867871ed3b5a67b81233d95fe65
Unknown <cupjoo@naver.com>
2020-05-12 23:51:38 +0900

    add handler for sign up form

---
+++ src/main/java/com/studyolle/account/SignUpFormValidator.java
@@ -0,0 +1,32 @@
+package com.studyolle.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@RequiredArgsConstructor
+@Component
+public class SignUpFormValidator implements Validator {
+
+    private final AccountRepository accountRepository;
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return clazz.isAssignableFrom(SignUpForm.class);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        SignUpForm signUpForm = (SignUpForm) target;
+        if(accountRepository.existsByEmail(signUpForm.getEmail())){
+            errors.rejectValue("email", "invaild.email",
+                    new Object[]{signUpForm.getEmail()}, "이미 사용 중인 이메일입니다.");
+        }
+
+        if(accountRepository.existsByNickname(signUpForm.getNickname())){
+            errors.rejectValue("nickname", "invaild.nickname",
+                    new Object[]{signUpForm.getNickname()}, "이미 사용 중인 닉네임입니다.");
+        }
+    }
+}
